1081f5e26a776b7a3bf990065d3a0676ac7b4c12
RLZW <rulazw@gmail.com>
2019-10-22 13:13:59 -0500

    Se añade la pantalla de Login

---
+++ app/src/main/java/mx/itesm/perdafirulais/LoginActivity.kt
@@ -0,0 +1,65 @@
+package mx.itesm.perdafirulais
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private var tag = "_Login"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btnRegistro.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
+        mAuth = FirebaseAuth.getInstance()
+
+        btnIngresar.setOnClickListener {
+            Log.d(tag, "Intentando Logear")
+            manejarLogin()
+        }
+    }
+
+    private fun manejarLogin() {
+        val mail = etMail.text.toString()
+        val password = etPassword.text.toString()
+        if (mail.isBlank() || password.isBlank()) {
+            Toast.makeText(
+                this, "Falta llenar alguno de los campos.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        login(mail, password)
+    }
+
+    private fun login(mail: String, password: String) {
+        mAuth.signInWithEmailAndPassword(mail, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(tag, "signInWithEmail:success")
+                    Toast.makeText(
+                        this, "Login Satisfactorio.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    startActivity(Intent(this, MainMenu::class.java))
+
+                } else {
+                    Toast.makeText(
+                        this, "No se pudo iniciar sesion. ${task.exception}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+}
---
+++ app/src/main/java/mx/itesm/perdafirulais/Register.kt
@@ -0,0 +1,12 @@
+package mx.itesm.perdafirulais
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
